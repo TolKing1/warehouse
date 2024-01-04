5c458489e6fa3124b817d2cc838e464f10c70fac
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-04 16:33:45 +0500

    test

--- src/main/java/uz/tolKing/warehouse/WareHouseApp.java
+++ src/main/java/uz/tolKing/warehouse/WareHouseApp.java
@@ -1,7 +1,13 @@
 package uz.tolKing.warehouse;
 
-public class WareHouseApp {
-    public static void main(String[] args) {
+import uz.tolKing.warehouse.dao.ConnectionDAO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class WareHouseApp {
+    public static void main(String[] args) throws SQLException {
+        ConnectionDAO db = new ConnectionDAO("postgres","123");
+        Connection con = db.getConnection();
     }
 }
