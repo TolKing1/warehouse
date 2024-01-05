c68c4cb786a781b67f674ed5ae3ef209f378412d
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-05 19:04:30 +0500

    ADD print and console

--- src/main/java/uz/tolKing/warehouse/controller/impl/ProductControllerImpl.java
+++ src/main/java/uz/tolKing/warehouse/controller/impl/ProductControllerImpl.java
@@ -1,21 +1,70 @@
 package uz.tolKing.warehouse.controller.impl;
 
 import uz.tolKing.warehouse.controller.Controller;
+import uz.tolKing.warehouse.controller.UserInteract;
+import uz.tolKing.warehouse.controller.util.DBTablePrinter;
+import uz.tolKing.warehouse.service.ProductService;
+
+import java.sql.Connection;
+import java.util.Scanner;
 
 public class ProductControllerImpl implements Controller {
+    private final Connection connection;
+    private final String tableName;
+    protected final UserInteract user = new UserInteract();
+    protected final Scanner scan = new Scanner(System.in);
+    String enterPrompt = """
+                                
+            Enter your prompt to filter data:
+            add <id> <name>... - to add item (make sure order and type are correct)
+            delete <id> - to delete item with given id
+            
+            search <attribute> <value> - to search ("name bowl")
+            sort <attribute> <order> - to sort ("name asc" or "price desc")
+            range <attribute> <min> <max> - to search by range ("price 2.99 12.5")
+            
+     
+            0 - to back main menu""";
 
-    @Override
-    public void print() {
-
+    public ProductControllerImpl(Connection connection,String tableName) {
+        this.connection = connection;
+        this.tableName = tableName;
     }
+
 
     @Override
     public void table() {
-
+        DBTablePrinter.printTable(this.connection,this.tableName);
     }
 
     @Override
     public void console() {
+        while (true) {
+            table();
+            //print instructions
+            user.printMsg(enterPrompt);
 
+            //split input
+            String[] next = scan.nextLine().trim().split(" ");
+
+            //exit
+            if (next.length == 1 && next[0].equals("0")) {
+                break;
+            }
+            //delete
+            else if (next.length == 2 && next[0].startsWith("del")) {
+                try {
+                    int id = Integer.parseInt(next[1]);
+
+                    ProductService.delete(tableName,id);
+                }catch (NumberFormatException e){
+                    user.printMsg("\n! Try with integer !");
+                }
+            }
+            //add
+            else if (next[0].startsWith("add")) {
+                ProductService.add(tableName,next);
+            }
+        }
     }
 }
