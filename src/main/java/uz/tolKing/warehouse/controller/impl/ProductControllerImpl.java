1e275a98e5a4efc4dc9d066e84b16b9d8b9f6cae
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-05 15:13:10 +0500

    NEW Product impl

---
+++ src/main/java/uz/tolKing/warehouse/controller/impl/ProductControllerImpl.java
@@ -0,0 +1,21 @@
+package uz.tolKing.warehouse.controller.impl;
+
+import uz.tolKing.warehouse.controller.Controller;
+
+public class ProductControllerImpl implements Controller {
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void table() {
+
+    }
+
+    @Override
+    public void console() {
+
+    }
+}
