ba29afeb7a71977d301ebd04ee92483d3123a04f
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-06 10:56:25 +0500

    IMPROVE readability and instructions

--- src/main/java/uz/tolKing/warehouse/controller/impl/ProductControllerImpl.java
+++ src/main/java/uz/tolKing/warehouse/controller/impl/ProductControllerImpl.java
@@ -15,12 +15,15 @@ public class ProductControllerImpl implements Controller {
     private final String tableName;
     String enterPrompt = """              
             Enter your prompt:
+            
             add <id> <name>... - to add item (make sure order and type are correct)
             delete <id> - to delete item with given id
                         
             search <attribute> <value> - example:       search name Cloth
             sort <attribute> <order> - example:         sort id asc
             range <attribute> <min> <max> - example:    range price 1 15
+            
+            P.s: Make sure you are using '.' instead of ',' for decimals
                         
             0 - to back main menu
             
--- src/main/java/uz/tolKing/warehouse/controller/util/DBTablePrinter.java
+++ src/main/java/uz/tolKing/warehouse/controller/util/DBTablePrinter.java
@@ -586,13 +586,6 @@ public class DBTablePrinter {
                 sj.add(name);
             }
 
-            String info = "Printing " + rowCount;
-            info += rowCount > 1 ? " rows from " : " row from ";
-            info += tableNames.size() > 1 ? "tables " : "table ";
-            info += sj.toString();
-
-            System.out.println(info);
-
             // Print out the formatted column labels
             System.out.print(strToPrint.toString());
 
