d62be777a46eb36d8942dfc64f3d4b9f25e7a14d
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-06 17:43:33 +0500

    ADD new method add which can create table

--- src/main/java/uz/tolKing/warehouse/controller/impl/CategoryController.java
+++ src/main/java/uz/tolKing/warehouse/controller/impl/CategoryController.java
@@ -1,15 +1,68 @@
 package uz.tolKing.warehouse.controller.impl;
 
 import uz.tolKing.warehouse.controller.Controller;
+import uz.tolKing.warehouse.controller.util.TableUtil;
+import uz.tolKing.warehouse.service.CategoryService;
+import uz.tolKing.warehouse.service.ConnectionService;
+
+import java.util.*;
 
 public class CategoryController implements Controller {
+    final CategoryService categoryService = new CategoryService();
+
+
     @Override
     public void table() {
+        List<String> tableList = ConnectionService.getTableNames();
+        String tablesString = TableUtil.tableListByOrder(tableList);
 
+        user.printMsg("Tables:\n%s".formatted(tablesString));
     }
 
     @Override
     public void console() {
+        main:
+        while (true) {
+            //print users
+            table();
 
+            //table name
+            user.printMsg("Enter name of category: <name> like 'pillow'\n\nLeave 0 or empty to quit");
+            String name = scan.nextLine().trim().toLowerCase();
+            //close
+            if (name.isEmpty() || name.equals("0")) {
+                break;
+            }
+            Map<String, String> params = new LinkedHashMap<>();
+
+            //params
+            while (true) {
+                user.printMsg("-".repeat(20));
+                user.printMsg("Your table:\n");
+                params.forEach((k, v) -> {
+                    System.out.printf(k + " " + v + " | ");
+                });
+                user.printMsg("\n");
+                user.printMsg("Enter data: <name> <type> like 'name text' \n\n Leave 0  to quit\n Leave empty to confirm");
+                String[] next = scan.nextLine().trim().split(" ");
+                //confirm
+                if (next[0].isEmpty()) {
+                    break;
+                }
+                //cancel all
+                else if (next.length == 1 && Objects.equals(next[0], "0")) {
+                    break main;
+                }
+                //add
+                else if (next.length == 2) {
+                    params.put(next[0], next[1]);
+                } else {
+                    user.printMsg("\n| Incorrect data. Try again like '<name> <type>'|\n");
+                }
+            }
+
+            categoryService.add(name,params);
+
+        }
     }
 }
--- src/main/java/uz/tolKing/warehouse/service/CategoryService.java
+++ src/main/java/uz/tolKing/warehouse/service/CategoryService.java
@@ -1,4 +1,69 @@
 package uz.tolKing.warehouse.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.util.Map;
+
+
 public class CategoryService {
+    final Connection connection;
+
+    public CategoryService() {
+        this.connection = ConnectionService.getConnection();
+    }
+    public void add(String tableName, Map<String,String> params) {
+        StringBuilder createTableQuery = new StringBuilder("CREATE TABLE ");
+        createTableQuery.append(tableName).append(" (");
+        createTableQuery.append("id").append(" ").append("SERIAL PRIMARY KEY").append(",");
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            createTableQuery.append(entry.getKey()).append(" ").append(entry.getValue()).append(",");
+        }
+
+        createTableQuery.deleteCharAt(createTableQuery.length() - 1); // Remove the last comma
+        createTableQuery.append(")");
+        boolean initialAutoCommit = false;
+        //get autocommit state
+        try {
+            initialAutoCommit = connection.getAutoCommit();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        Savepoint savepoint = null;
+        try {
+            //Manual commit
+            try {
+                connection.setAutoCommit(false);
+                savepoint = connection.setSavepoint();
+
+                System.out.println("|".repeat(25));
+                try {
+                    Statement statement = connection.createStatement();
+                    statement.execute(createTableQuery.toString());
+                    System.out.printf("Table has been successfully added%n");
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("|".repeat(25));
+
+
+                connection.commit();
+            } catch (SQLException e) {
+                rollbackConnection(connection, savepoint);
+            } finally {
+                connection.setAutoCommit(initialAutoCommit);
+            }
+        } catch (SQLException e) {
+            System.out.println("\n! Try again !\n");
+        }
+    }
+    private static void rollbackConnection(Connection connection, Savepoint savepoint) throws SQLException {
+        if (savepoint != null) {
+            connection.rollback(savepoint);
+        } else {
+            connection.rollback();
+        }
+    }
+
 }
