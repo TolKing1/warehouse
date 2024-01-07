654dac699b8452518078f059b73717caec066d34
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-01-07 17:42:08 +0500

    FIX syntax error

--- db.sql
+++ db.sql
@@ -1,11 +1,10 @@
-CREATE ROLE if not exists guest WITH LOGIN PASSWORD 'guest';
+CREATE ROLE guest WITH LOGIN PASSWORD 'guest';
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO guest;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO guest;
 
 create table if not exists dish
 (
-    id       integer default nextval('"Dish_id_seq"'::regclass) not null
-        constraint "Dish_pkey"
+    id       serial
             primary key,
     name     varchar(200),
     price    numeric(10, 2),
@@ -15,8 +14,7 @@ create table if not exists dish
 );
 create table if not exists linen
 (
-    id        integer default nextval('"Linen_id_seq"'::regclass) not null
-        constraint "Linen_pkey"
+    id        serial
             primary key,
     name      varchar(200),
     price     numeric(10, 2),
